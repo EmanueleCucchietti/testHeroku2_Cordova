d8da6e68976548543d0f9fec9df0ce7f79395d1e
EmanueleCucchietti <e.cucchietti.1732@vallauri.edu>
2022-02-02 17:32:01 +0100

    Access Control List Enabled To *

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { json } = require("express/lib/response");
 const http = require("http");
 
 const PORT = process.env.PORT || 5000;
@@ -22,6 +21,28 @@ app.use("/", function (req, res, next) {
     console.log(req.method + " : " + req.originalUrl);
     next();
 });
+// ----- -> ACCESS CONTROL LIST <- ----- //
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', "*");
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+// ----- -> ACCESS CONTROL LIST <- ----- //
+
+
 // route risorse statiche
 app.use("/", express.static('./static'));
 
